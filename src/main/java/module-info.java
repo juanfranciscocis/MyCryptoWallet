88394dab795b3716d2d79419698cd684d4b5c575
JuanFranciscoCis <juanfrancistm2011@icloud.com>
2022-07-31 23:25:15 -0500

    MAIN MENU LOADS

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,4 +5,6 @@ module com.example.mycryptowallet2_0 {
 
     opens com.example.mycryptowallet2_0 to javafx.fxml;
     exports com.example.mycryptowallet2_0;
+    exports com.example.mycryptowallet2_0.FML_CONTROLLERS;
+    opens com.example.mycryptowallet2_0.FML_CONTROLLERS to javafx.fxml;
 }
